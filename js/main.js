7f9244d2c81d8b111b751c3081f7cf4e348bc293
yubarajsingh <yubaraj605@gmail.com>
2021-07-08 15:28:47 +0545

    initail

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+var data
+
+const tab=document.getElementById('tableBody');
+function toTimestamp(strDate){
+    var datum = Date.parse(strDate);
+    return datum/1000;
+ }
+
+const calling =async()=>{
+    let tableBody = '';
+    const response= await fetch('https://hdiapi.azurewebsites.net/list/')
+    data= await response.json()
+    
+    
+    data.slice(-5).reverse().forEach(da=>{
+       let date=toTimestamp(da.date)
+        
+        
+       tableBody+= `<tr scope="row">                                  
+        <td>
+          ${da.id}
+        </td>
+        <td> ${da.qr}</td>
+        <td>
+        ${da.date}
+        
+        </td>        
+        </tr>`
+
+    })
+
+    tab.innerHTML=tableBody
+    
+}
+
+setInterval(calling,1000)
